cb0f8012d0873050954c5c3fc371fdb5a29945a6
dhegeraad <danny@jsks-swo.nl>
2019-02-11 11:46:38 +0100

    test

--- dist/ChartService.d.ts
+++ dist/ChartService.d.ts
@@ -1,5 +1,10 @@
-export declare class ChartService implements IChartService {
-    renderDonutDiagram(): string;
-    EMOJI_SHORTCODES: RegExp;
-    findEmojis(str: string): string;
+export declare module Chart {
+    interface IChartService {
+        renderDonutDiagram(): string;
+    }
+    class ChartService implements IChartService {
+        renderDonutDiagram(): string;
+        EMOJI_SHORTCODES: RegExp;
+        findEmojis(str: string): string;
+    }
 }
--- src/ChartService.ts
+++ src/ChartService.ts
@@ -1,16 +1,22 @@
-export class ChartService implements IChartService  {
-    public renderDonutDiagram(): string {
-        return "this is a donut";
+export module Chart {
+    export interface IChartService {
+        renderDonutDiagram(): string;
     }
+    
+    export class ChartService implements IChartService  {
+        public renderDonutDiagram(): string {
+            return "this is a donut";
+        }
 
-    public EMOJI_SHORTCODES = /:[a-zA-Z1-9_]+:/g
+        public EMOJI_SHORTCODES = /:[a-zA-Z1-9_]+:/g
 
-    public findEmojis(str: string): string {
-    // add runtime check for use in JavaScript
-    if (typeof str !== 'string') {
-        return "";
-    }
+        public findEmojis(str: string): string {
+        // add runtime check for use in JavaScript
+        if (typeof str !== 'string') {
+            return "";
+        }
 
-    return "blaat";
+        return "blaat";
+        }
     }
 }
